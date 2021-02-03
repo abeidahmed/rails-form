20e3d069921415766ea512d1cf5145983cc7b21b
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 14:04:23 +0530

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  before_validation :normalize_fields
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :username, :email_address, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
+  validates :email_address, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+  def normalize_fields
+    self.email_address = email_address.to_s.downcase.strip
+    self.username = username.to_s.downcase.strip
+  end
 end
