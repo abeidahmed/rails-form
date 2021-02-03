8d023a9c2bb6a5b6b87e66f4e0db8068ee308e2d
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 16:35:45 +0530

    Set user using before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.all
   end
@@ -17,17 +19,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to @user
     else
@@ -36,6 +32,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email_address, :password)
