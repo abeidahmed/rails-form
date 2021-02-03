025f851282df28619d30cdcf2c5d6d9f2a650e6f
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 15:21:34 +0530

    Show validation errors on the user create form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      redirect_to user
+    if @user.save
+      redirect_to @user
     else
       render :new
     end
