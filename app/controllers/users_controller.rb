aadc04d94f82b5658fb9691e8199f3caacb020ca
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 16:34:19 +0530

    Create user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
   def edit
     @user = User.find(params[:id])
