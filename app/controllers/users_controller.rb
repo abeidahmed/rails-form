2a72c5e2181e852e8d74abdcab88292fad127d3c
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 15:28:32 +0530

    Add users edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,20 @@ class UsersController < ApplicationController
 
   def show; end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
