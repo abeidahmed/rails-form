744cdac14bd8f8ee780ef0a260dab7a80ea057a6
abeidahmed <abeidahmed92@gmail.com>
2021-02-03 14:47:22 +0530

    Switch to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_02_03_081821) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email_address", default: "", null: false
